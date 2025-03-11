a8052eb7d1628844f18d0c65014cbd4539b5f6e8
Dave For Projects <daveforprojects@gmail.com>
2025-03-11 19:54:07 -0400

    Golden ratio

--- courses/haskell_mooc/Gold.hs
+++ courses/haskell_mooc/Gold.hs
@@ -1,12 +1,14 @@
 -- To run: stack runhaskell Gold.hs
 
-
+-- Limit to one module per file
 module Gold where
 
 -- Golden ratio
+-- Define a constant, phi. The type annotation is optional
 phi :: Double
 phi = (sqrt 5 + 1) / 2
 
+-- Define a function. Left side requires a parameter
 polynomial :: Double -> Double
 polynomial x = x^2 - x - 1
 
