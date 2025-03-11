1267b65b107e3d387502c1cefe761d7ad43b8569
Dave For Projects <daveforprojects@gmail.com>
2025-03-11 19:12:45 -0400

    Golden ratio

---
+++ courses/haskell_mooc/Gold.hs
@@ -0,0 +1,17 @@
+-- To run: stack runhaskell Gold.hs
+
+
+module Gold where
+
+-- Golden ratio
+phi :: Double
+phi = (sqrt 5 + 1) / 2
+
+polynomial :: Double -> Double
+polynomial x = x^2 - x - 1
+
+f x = polynomial (polynomial x)
+
+main = do
+    print (polynomial phi)
+    print (f phi)
