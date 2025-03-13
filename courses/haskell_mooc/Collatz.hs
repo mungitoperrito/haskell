29538eec129f20ce144eb94cd76e241194e78080
Dave For Projects <daveforprojects@gmail.com>
2025-03-13 11:17:56 -0400

    Collatz sequence

--- courses/haskell_mooc/Collatz.hs
+++ courses/haskell_mooc/Collatz.hs
@@ -4,17 +4,17 @@
 module Collatz where
 
 -- One step in the sequence
-step :: Integer -. Integer
+step :: Integer -> Integer
 
 -- Indent is 4 spaces, except where is 2
-step x = if even x then stepDown else StepUp
+step x = if even x then stepDown else stepUp
   where stepDown = div x 2
         stepUp = 3 * x + 1
 
 -- Count the number of steps
-collataz :: Integer -> Integer
-collataz 1 = 0
-collataz x = 1 + collataz (step x)
+collatz :: Integer -> Integer
+collatz 1 = 0
+collatz x = 1 + collatz (step x)
 
 -- Longest sequence between 0 and upperBound
 longestSequence :: Integer -> Integer
@@ -29,4 +29,4 @@ longestSequence' number maxLength n =
   if len > maxLength
   then longestSequence' n len (n-1)
   else longestSequence' number maxLength (n-1)
-  where len = collataz n
+  where len = collatz n
