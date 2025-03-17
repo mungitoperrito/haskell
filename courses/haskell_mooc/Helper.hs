7255b4742c3cdcef20f5d04c9e10913e9dde8554
Dave For Projects <daveforprojects@gmail.com>
2025-03-17 16:13:56 -0400

    Ch 02

--- courses/haskell_mooc/Helper.hs
+++ courses/haskell_mooc/Helper.hs
@@ -21,7 +21,19 @@ repeatHelpPattern 0 str result = result
 repeatHelpPattern n str result = repeatHelpPattern (n-1) str (result ++ str)
 
 
+-- Fibonacci sequence
+fibonacci :: Integer -> Integer
+fibonacci n = fibonacciHelp 0 1 n
+
+fibonacciHelp :: Integer -> Integer -> Integer -> Integer
+fibonacciHelp a b 1 = b
+fibonacciHelp a b n = fibonacciHelp b (a+b) (n-1)
+
 
 main = do
     print (repeatStringIf 3 "OoO")
     print (repeatStringPattern 3 "OoO")
+    print (fibonacci 3)
+    print (fibonacci 4)
+    print (fibonacci 5)
+    print (fibonacci 10)
